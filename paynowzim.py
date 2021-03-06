1d2122a95682ab7e1ac9129d80dfe9df9f7ba32d
Beven Nyamande <bevennyamande@gmail.com>
2021-03-06 11:35:19 +0200

    Refactored code

--- paynowzim.py
+++ paynowzim.py
@@ -4,17 +4,20 @@ from six.moves.urllib_parse import quote_plus, parse_qs
 
 
 class Paynow(object):
-    '''Instantiates the Paynow Object'''
+    """Instantiates the Paynow Object"""
 
-    WEB_URL: str = 'https://www.paynow.co.zw/interface/initiatetransaction'
-    MOBILE_URL: str = 'https://www.paynow.co.zw/interface/remotetransaction'
+    WEB_URL: str = "https://www.paynow.co.zw/interface/initiatetransaction"
+    MOBILE_URL: str = "https://www.paynow.co.zw/interface/remotetransaction"
 
-    def __init__(self, pid: str='',
-                 pkey: str='',
-                 returnurl: str='',
-                 resulturl: str='',
-                 authemail: str='',
-                 phone: str=None):
+    def __init__(
+        self,
+        pid: str = "",
+        pkey: str = "",
+        returnurl: str = "",
+        resulturl: str = "",
+        authemail: str = "",
+        phone: str = None,
+    ):
 
         self.pid = pid
         self.pkey = pkey
@@ -25,15 +28,13 @@ class Paynow(object):
         self.reference = ""
         self.products: dict = {}
         self.frompaynow: dict = {}
-        self.method : str = "ecocash"
-
+        self.method: str = "ecocash"
 
     def set_response_from_paynow(self, resp) -> dict:
-        ''' Setter for response from Paynow '''
+        """ Setter for response from Paynow """
 
         self.frompaynow = self.decrypt(resp)
         return self.frompaynow
-
 
     def total(self) -> float:
         """ calculate the total of products in cart to two decimal point """
@@ -42,28 +43,27 @@ class Paynow(object):
             ctx.prec = 2
         amount = sum(value for value in self.products.values())
         if amount <= 1:
-            raise ValueError('Payments less than $1 are disallowed!')
-        return float(Decimal(amount).quantize(Decimal('.00')))
-
+            raise ValueError("Payments less than $1 are disallowed!")
+        return float(Decimal(amount).quantize(Decimal(".00")))
 
     def send_payment(self, reference, products) -> dict:
-        ''' Sends the transaction to Paynow'''
+        """ Sends the transaction to Paynow"""
 
         data = self.build_request(reference, products)
         try:
-            if data['method']: # for mobile payments
+            if data["method"]:  # for mobile payments
                 return self.set_response_from_paynow(
-                        requests.post(self.MOBILE_URL, data=data))
+                    requests.post(self.MOBILE_URL, data=data)
+                )
 
             # URL that handles web payments
-            return self.set_response_from_paynow(requests.post(
-                                       self.WEB_URL, data=data))
+            return self.set_response_from_paynow(requests.post(self.WEB_URL, data=data))
         except Exception as e:
             raise e
 
     def status_update(self):
-        pollurl = self.frompaynow.get('pollurl', '')
-        data = requests.post(pollurl) if pollurl else ''
+        pollurl = self.frompaynow.get("pollurl", "")
+        data = requests.post(pollurl) if pollurl else ""
         try:
             return self.decrypt(data)
         except Exception:
@@ -73,12 +73,12 @@ class Paynow(object):
     def encrypt(self, data) -> str:
         message = ""
         for key, value in data.items():
-            if(str(key).lower() == 'hash'):
+            if str(key).lower() == "hash":
                 continue
 
             message += str(value)
         message += self.pkey
-        return hashlib.sha512(message.encode('utf-8')).hexdigest().upper()
+        return hashlib.sha512(message.encode("utf-8")).hexdigest().upper()
 
     def decrypt(self, data) -> dict:
         # TODO check functionality here
@@ -88,24 +88,30 @@ class Paynow(object):
             print(e)
 
     def build_request(self, reference, products):
-        ''' Build the required post object for Paynow API'''
+        """ Build the required post object for Paynow API"""
 
         self.reference = reference
         for key, amount in products.items():
             self.products[key] = float(amount)
 
-        body = {'id':self.pid, 'reference':self.reference,
-                'amount':self.total(), 'additionalinfo':'xyz',
-                'returnurl':self.returnurl, 'resulturl':self.resulturl,
-                'authemail':self.authemail, 'phone':self.phone,
-                'method':self.method, 'Status':'Message'
-                }
+        body = {
+            "id": self.pid,
+            "reference": self.reference,
+            "amount": self.total(),
+            "additionalinfo": "xyz",
+            "returnurl": self.returnurl,
+            "resulturl": self.resulturl,
+            "authemail": self.authemail,
+            "phone": self.phone,
+            "method": self.method,
+            "Status": "Message",
+        }
 
         for key, value in body.items():
-            if (key == 'authemail'):
+            if key == "authemail":
                 continue
 
             body[key] = quote_plus(str(value))
 
-        body['Hash'] = self.encrypt(body)
+        body["Hash"] = self.encrypt(body)
         return body
--- setup.py
+++ setup.py
@@ -3,26 +3,25 @@
 from setuptools import setup
 
 setup(
-    name='paynowzim',
-    version='2.0.0',
-    description='A minimalist python wrapper for the Paynow Payment Gateway.',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    author='Beven Nyamande',
-    author_email='bevenfx@gmail.com',
-    url='http://www.github.com/bevennyamande/paynowzw',
-    py_modules=['paynowzw'],
+    name="paynowzim",
+    version="2.0.0",
+    description="A minimalist python wrapper for the Paynow Payment Gateway.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Beven Nyamande",
+    author_email="bevenfx@gmail.com",
+    url="http://www.github.com/bevennyamande/paynowzw",
+    py_modules=["paynowzw"],
     include_package_data=True,
     install_requires=[
-    'requests',
-]
-,
-    license='MIT',
+        "requests",
+    ],
+    license="MIT",
     classifiers=[
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python ',
-        'Programming Language :: Python :: 3.4 ',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.8',
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python ",
+        "Programming Language :: Python :: 3.4 ",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.8",
     ],
 )
